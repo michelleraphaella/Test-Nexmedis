a360005201057bc9ecdf12566fb531a69577eda1
michelleraphaella <michelle.raphaella31@gmail.com>
2024-08-05 21:00:54 +0700

    Vue

---
+++ nexmedis-michelle/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<template>
+
+<router-view/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ nexmedis-michelle/src/views/HomeView.vue
@@ -0,0 +1,285 @@
+<script>
+</script>
+<template>
+    <header>
+        <nav>
+            <ul>
+                <li class="logo bold">LOGO</li>
+                <li>
+                <span>Service Category 1</span>
+                <ul class="dropdown">
+                    <li><span>Service 1</span></li>
+                    <li><span>Service 2</span></li>
+                    <li><span>Service 3</span></li>
+                </ul>
+                </li>
+                <li>
+                <span>Service Category 2</span>
+                <ul class="dropdown">
+                    <li><span>Service 1</span></li>
+                    <li><span>Service 2</span></li>
+                    <li><span>Service 3</span></li>
+                </ul>
+                </li>
+                <li><button type="button">Sign In</button></li>
+                <li><button type="button">Sign Up</button></li>
+                <li><button type="button" class="bold">Become a Seller</button></li>
+            </ul>
+        </nav>
+    </header>
+    <body class="body">
+        <div class="container">
+            <h1>We're Here To Make <br> Your Life Easier.</h1>
+            <div class="search-container">
+                <input type="text" name="text" value="">
+                <button type="button">Search</button>
+            </div>
+        </div>
+        <div class="container2">
+            <h2>Heading Line</h2>
+            <p>Lörem ipsum kvasirtad sön huruvida pohönera set. Båbire ultragen homodegon: deneheten ifall geoliga.
+             Lyng kada. Vass fade stenoska. Etånt dossa alltså hypode vikydade.</p>
+        </div>
+        <div class="container3">
+            <div class="card">
+                <img src="img/bg.jpg" alt="Image" class="circle">
+                <h3>Heading Line</h3>
+                <p>Lörem ipsum kvasirtad sön huruvida pohönera set. Båbire ultragen </p>
+            </div>
+            <div class="card">
+                <img src="img/bg.jpg" alt="Image" class="circle">
+                <h3>Heading Line</h3>
+                <p>Lörem ipsum kvasirtad sön huruvida pohönera set. Båbire ultragen </p>
+            </div>
+            <div class="card">
+                <img src="img/bg.jpg" alt="Image" class="circle">
+                <h3>Heading Line</h3>
+                <p>Lörem ipsum kvasirtad sön huruvida pohönera set. Båbire ultragen </p>
+            </div>
+            <div class="card">
+                <img src="img/bg.jpg" alt="Image" class="circle">
+                <h3>Heading Line</h3>
+                <p>Lörem ipsum kvasirtad sön huruvida pohönera set. Båbire ultragen </p>
+            </div>
+        </div>
+        <div class="container4">
+            <h2>Popular Services</h2>
+            <div id="slider">
+                <input type="radio" name="slider" id="slide1" checked>
+                <input type="radio" name="slider" id="slide2">
+                <div id="slides">
+                    <div id="overflow">
+                        <div class="inner">
+                            <div class="slide slide_1">
+                                <div class="slide-content">
+                                    <div class="container-img">
+                                        <div class="img">
+                                            <img src="img/bg.jpg" alt="Image" class="rect">
+                                        </div>
+                                        <div class="img">
+                                            <img src="img/bg.jpg" alt="Image" class="rect">
+                                        </div>
+                                        <div class="img">
+                                            <img src="img/bg.jpg" alt="Image" class="rect">
+                                        </div>
+                                        <div class="img">
+                                            <img src="img/bg.jpg" alt="Image" class="rect">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="slide slide_2">
+                                <div class="slide-content">
+                                    <div class="container-img">
+                                        <div class="img">
+                                            <img src="img/logo.png" alt="Image" class="rect">
+                                        </div>
+                                        <div class="img">
+                                            <img src="img/logo.png" alt="Image" class="rect">
+                                        </div>
+                                        <div class="img">
+                                            <img src="img/logo.png" alt="Image" class="rect">
+                                        </div>
+                                        <div class="img">
+                                            <img src="img/logo.png" alt="Image" class="rect">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="controls">
+                    <label for="slide1"></label>
+                    <label for="slide2"></label>
+                </div>
+                <div id="bullets">
+                    <label for="slide1"></label>
+                    <label for="slide2"></label>
+                </div>
+            </div>
+        </div>
+        <div class="container5">
+            <h2>Nearby Companies</h2>
+            <div class="container-img">
+                <div class="img">
+                    <div class="image">
+                        <img src="img/bg.jpg" alt="Image" class="circle">
+                        <p>Lörem ipsum kvasirtad sön huruvida pohönera</p>
+                    </div>
+                    <h3>About</h3>
+                    <p>Lörem ipsum kvasirtad sön huruvida pohönera set. Båbire ultragen Lörem ipsum kvasirtad sön huruvida</p>
+                    <button type="button">Message</button>
+                    <button type="button">View More</button>
+                </div>
+                <div class="img">
+                    <div class="image">
+                        <img src="img/bg.jpg" alt="Image" class="circle">
+                        <p>Lörem ipsum kvasirtad sön huruvida pohönera</p>
+                    </div>
+                    <h3>About</h3>
+                    <p>Lörem ipsum kvasirtad sön huruvida pohönera set. Båbire ultragen Lörem ipsum kvasirtad sön huruvida</p>
+                    <button type="button">Message</button>
+                    <button type="button">View More</button>
+                </div>
+                <div class="img">
+                    <div class="image">
+                        <img src="img/bg.jpg" alt="Image" class="circle">
+                        <p>Lörem ipsum kvasirtad sön huruvida pohönera</p>
+                    </div>
+                    <h3>About</h3>
+                    <p>Lörem ipsum kvasirtad sön huruvida pohönera set. Båbire ultragen Lörem ipsum kvasirtad sön huruvida</p>
+                    <button type="button">Message</button>
+                    <button type="button">View More</button>
+                </div>
+            </div>
+        </div>
+        <div class="container6">
+            <h2>Most Needed Home Services</h2>
+            <div class="container-img">
+                <div class="img">
+                    <img src="img/bg.jpg" alt="Image" class="full">
+                    <div class="image">
+                        <img src="img/bg.jpg" alt="Image" class="circle">
+                        <p>Lörem ipsum kvasirtad sön huruvida pohönera set. Båbire ultragen Lörem ipsum kvasirtad sön huruvida</p>
+                    </div>
+                    <button type="button">Message</button>
+                    <button type="button">View More</button>
+                </div>
+                <div class="img">
+                    <img src="img/bg.jpg" alt="Image" class="full">
+                    <div class="image">
+                        <img src="img/bg.jpg" alt="Image" class="circle">
+                        <p>Lörem ipsum kvasirtad sön huruvida pohönera set. Båbire ultragen Lörem ipsum kvasirtad sön huruvida</p>
+                    </div>
+                    <button type="button">Message</button>
+                    <button type="button">View More</button>
+                </div>
+                <div class="img">
+                    <img src="img/bg.jpg" alt="Image" class="full">
+                    <div class="image">
+                        <img src="img/bg.jpg" alt="Image" class="circle">
+                        <p>Lörem ipsum kvasirtad sön huruvida pohönera set. Båbire ultragen Lörem ipsum kvasirtad sön huruvida</p>
+                    </div>
+                    <button type="button">Message</button>
+                    <button type="button">View More</button>
+                </div>
+                <div class="img">
+                    <img src="img/bg.jpg" alt="Image" class="full">
+                    <div class="image">
+                        <img src="img/bg.jpg" alt="Image" class="circle">
+                        <p>Lörem ipsum kvasirtad sön huruvida pohönera set. Båbire ultragen Lörem ipsum kvasirtad sön huruvida</p>
+                    </div>
+                    <button type="button">Message</button>
+                    <button type="button">View More</button>
+                </div>
+            </div>
+        </div>
+        <div class="container7">
+            <h2>View Safety Tips</h2>
+            <div class="card-cont">
+                <div class="card">
+                    <p>Lörem ipsum kvasirtad sön huruvida pohönera set. Båbire ultragen Lörem ipsum kvasirtad sön huruvida</p>
+                    <button type="button">Learn More</button>
+                </div>
+                <div class="card">
+                    <p>Lörem ipsum kvasirtad sön huruvida pohönera set. Båbire ultragen Lörem ipsum kvasirtad sön huruvida</p>
+                    <button type="button">Learn More</button>
+                </div>
+            </div>
+        </div>
+        <div class="container8">
+            <div class="heading-left">
+                <h2>Heading</h2>
+                <div class="about">
+                    <h3>About</h3>
+                    <p>Lörem ipsum kvasirtad sön huruvida pohönera set. Båbire ultragen Lörem ipsum kvasirtad sön huruvida</p>
+                </div>
+                <div class="about">
+                    <h3>About</h3>
+                    <p>Lörem ipsum kvasirtad sön huruvida pohönera set. Båbire ultragen Lörem ipsum kvasirtad sön huruvida</p>
+                </div>
+                <div class="contractor">
+                    <button type="button">Book a Contractor</button>
+                </div>
+            </div>
+            <img src="img/bg.jpg" alt="Image" class="rect">
+        </div>
+        <div class="container9">
+            <img src="img/bg.jpg" alt="Image" class="rect">
+            <div class="heading-left">
+                <h2>Heading</h2>
+                <div class="about">
+                    <h3>About</h3>
+                    <p>Lörem ipsum kvasirtad sön huruvida pohönera set. Båbire ultragen Lörem ipsum kvasirtad sön huruvida</p>
+                </div>
+                <div class="about">
+                    <h3>About</h3>
+                    <p>Lörem ipsum kvasirtad sön huruvida pohönera set. Båbire ultragen Lörem ipsum kvasirtad sön huruvida</p>
+                </div>
+                <div class="contractor">
+                    <button type="button">Become a Seller</button>
+                </div>
+            </div>
+        </div>
+        <div class="container10">
+            <h2 class="comp">100+ Registered <br> Companies</h2>
+            <h2 class="memb">2999 Registered <br> Members</h2>
+        </div>
+        <div class="container11">
+            <div class="footer-cont">
+                <div class="footer1">
+                    <h2 class="bold log">LOGO</h2>
+                    <p class="p-log">Lörem ipsum kvasirtad sön huruvida pohönera set. Båbire ultragen Lörem ipsum kvasirtad sön huruvida</p>
+                </div>
+                <div class="footer1">
+                    <h2 class="bold">Lorem Epsum</h2>
+                    <p class="p">Lörem ipsum kvasirtad sön huruvida pohönera set. Båbire ultragen Lörem ipsum kvasirtad sön huruvida</p>
+                </div>
+                <div class="footer1">
+                    <h2 class="bold">Lorem Epsum</h2>
+                    <p class="p">Lörem ipsum kvasirtad sön huruvida pohönera set. Båbire ultragen Lörem ipsum kvasirtad sön huruvida</p>
+                </div>
+            </div>
+            <div class="icon-cont">
+                <img src="img/bg.jpg" alt="Image" class="circle">
+                <img src="img/bg.jpg" alt="Image" class="circle">
+                <img src="img/bg.jpg" alt="Image" class="circle">
+            </div>
+            <div class="btn">
+                <button type="button">Message</button>
+                <button type="button">View More</button>
+            </div>
+        </div>
+    </body>
+    <footer>
+        <div>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+  name: 'HomeView'
+};
+</script>
